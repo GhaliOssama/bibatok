47c760f22342399e762579d509b64020ee729e81
GhaliOssama <109102335+GhaliOssama@users.noreply.github.com>
2022-10-23 14:56:28 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,8 @@
-var cacheName = 'bibatok';
-var filesToCache = [
-  '/',
-  '/index.html',
-  '/css/styles.css',
-  '/js/main.js'
-];
+window.onload = () => {
+  'use strict';
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
-  self.skipWaiting();
-});
-
-/* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
-});
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./sw.js');
+  }
+}
